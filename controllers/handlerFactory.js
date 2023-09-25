930477dfc5a8658a0f3c611482a09fb34ac3a69e
Raviraj <ravirajghodake98@gmail.com>
2023-09-25 19:00:10 +0530

    handler factory

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -1,10 +1,7 @@
-//factory function is the function which returns another function
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
 const APIFeatures = require('./../utils/apiFeatures');
 
-//we'll pass the model into this function and then we create a new function & that function will return our async function
-//this works bcoz of the JS closure; which is that this inner function here will get access to the varibles of the outer function, even after the outer fn has already been returned
 exports.deleteOne = Model => catchAsync(async (req, res, next) => {
   const doc = await Model.findByIdAndDelete(req.params.id);
 
@@ -18,9 +15,7 @@ exports.deleteOne = Model => catchAsync(async (req, res, next) => {
   });
 });
 
-
 exports.updateOne = Model => catchAsync(async (req, res, next) => {
-  //whenever we use findByIdAndUpdate, all the save middleware will not run
   const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
     runValidators: true
@@ -38,7 +33,6 @@ exports.updateOne = Model => catchAsync(async (req, res, next) => {
   })
 });
 
-
 exports.createOne = Model => catchAsync(async (req, res, next) => {
   const doc = await Model.create(req.body);
   res.status(201).json({
@@ -49,16 +43,10 @@ exports.createOne = Model => catchAsync(async (req, res, next) => {
   })
 });
 
-
-//instead of simply passing the model, we'll pass the populate option too
 exports.getOne = (Model, popOptions) => catchAsync(async (req, res, next) => {
-  //we'll first create a query and if there is a popOptions object is available, we'll add that to query and by end, we'll await that query
   let query = Model.findById(req.params.id);
   if (popOptions) query = query.populate(popOptions);
-  //this logic that we have above with not awaiting the query, but instead saving it in the variable, so that in next step, we can manipulate it
   const doc = await query;
-
-  // const doc = await Model.findById(req.params.id).populate('reviews');
 
   if (!doc) {
     return next(new AppError('No document found with that ID', 404));
@@ -72,20 +60,16 @@ exports.getOne = (Model, popOptions) => catchAsync(async (req, res, next) => {
   })
 });
 
-
 exports.getAll = Model => catchAsync(async (req, res, next) => {
-  // Tp allow for nested GET reviews on tour (hack)
   let filter = {};
   if (req.params.tourId) filter = { tour: req.params.tourId };
 
-  //the above filter will then pass it in here
   const features = new APIFeatures(Model.find(filter), req.query)
     .filter()
     .sort()
     .limitFields()
     .paginate();
-  //mongo automatically creates an index on the id field by default itself
-  // const doc = await features.query.explain();
+
   const doc = await features.query;
 
   res.status(200).json({
