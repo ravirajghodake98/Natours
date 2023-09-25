a10f9191290abad1016194f7522e2a5369a2c4bd
Raviraj <ravirajghodake98@gmail.com>
2023-09-25 18:56:07 +0530

    bookingController

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,5 +1,4 @@
-// we are passing a stripe secret key right here, which will then give us a stripe object that we can work with
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);    //this npm package only works for backend
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const Tour = require('./../models/tourModel');
 const User = require('./../models/userModel');
@@ -12,45 +11,25 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.tourId);
 
   //2) Create checkout session
-  //information about the session
   const session = await stripe.checkout.sessions.create({
-    payment_method_types: ['card'],    //array of payment methods and card is for credit card
+    payment_method_types: ['card'],
 
-    // success_url: `${req.protocol}://${req.get('host')}/`,    //url that will get called as soon as the credit card has been successfully charged(user will be redirected to this url)
-    //we need to create a query string to below url bcoz stripe will just make a get request to this url here, so we cannot send any data with body except the query string
-    //this is not secure at all bcoz anyone who knows the url structure could simply call it w/o going through the checkout process
-    //means anyone can book a tour w/o even paying
-    // success_url: `${req.protocol}://${req.get('host')}/my-tours/?tour=${req.params.tourId}&user=${req.user.id}&price=${tour.price}`,
-
-    // success_url: `${req.protocol}://${req.get('host')}/my-tours`,
     success_url: `${req.protocol}://${req.get('host')}/my-tours?alert=booking`,
 
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
-    customer_email: req.user.email,   //so that user doesn't need to fill in checkout
-    //this field will allow us to pass in some data that we are currently creating, and it's imp bcoz once the purchase was successful, we'll get access to session object again
+    customer_email: req.user.email,
     client_reference_id: req.params.tourId,
 
-    //information about the product
     line_items: [
       {
-        //all these field names(name, description,... comes from stripe, we cannot make up our own fields)
-        // name: `${tour.name} Tour`,
-        // description: tour.summary,
-        // //these array of images should be the live images(hosted on internet) bcoz stripe will upload them to their own server
-        // images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
-        // amount: tour.price * 100,    //bcoz the amount is in cent/paise
-        // currency: 'usd',
-        // quantity: 1,
-
         price_data: {
           currency: 'usd',
           product_data: {
             name: `${tour.name} Tour`,
             description: tour.summary,
-            // images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
             images: [`${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`],
           },
-          unit_amount: tour.price * 100,    //bcoz the amount is in cent/paise
+          unit_amount: tour.price * 100,
         },
         quantity: 1
       }
@@ -65,21 +44,8 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   })
 });
 
-// exports.createBookingCheckout = catchAsync(async (req, res, next) => {
-//   //This is only TEMPORARY, bcoz it's UNSECURE; everyone can make booking w/o paying
-//   const { tour, user, price } = req.query;
-
-//   if (!tour && !user && !price) return next();
-//   await Booking.create({ tour, user, price });
-
-//   //to make the url(success_url) more secure, we'll remove(or hide) the query string from it
-//   //redirect here will basically create a new request but to this new url that we passed in there
-//   res.redirect(req.originalUrl.split('?')[0]);
-// })
-
 const createBookingCheckout = async session => {
   const tour = session.client_reference_id;
-  //by wrapping all this in parenthesis and calling id on it will return only the id
   const user = (await User.findOne({ email: session.customer_email })).id;
   const price = session.display_items[0].unit_amount / 100;
 
